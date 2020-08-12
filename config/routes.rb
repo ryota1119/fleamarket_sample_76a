2221c49501b8eac9935d930ffd61fe2569b691f9
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-08-12 13:40:25 +0900

    create htmlfile & edit routes.rb
    logout.html.haml
    credit.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources  :users, only: :show
+  resources  :users, only: [:show, :destroy] do
+    member do
+      get :logout
+      get :credit
+    end
+  end
 end
