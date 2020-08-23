e4d59f260bd7bfa0abba7cc46ae5c2311e529f02
fumn112026 <fumn112026@gmail.com>
2020-08-23 15:36:45 +0900

    create_method, view, routing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+    @item.item_images.new
+    @category_parent_array = ["---"]
+    @category_parent_array = Category.where(ancestry: nil)
+  end
+
+  def get_category_children
+  @category_children = Category.find(params[:parent_id]).category_children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:children_id]).children_id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+
+  resources :items do
+    collection do
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
+  
   resources  :users, only: [:show, :destroy] do
     member do
       get :logout
