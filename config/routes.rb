11b24bab08528ceff4668de322d5681f688f199b
fumn112026 <fumn112026@gmail.com>
2020-08-28 13:57:31 +0900

    make_routing,controller
    imagesコントローラーを作成。itemを紐付けてルーティングを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
-  resources :items, only: [:new, :create]
+  root to: 'items#index'
+  resources :items, only: [:new, :create] do
+    resources :images, only: :create
+  end
   resources  :users, only: [:show, :destroy] do
     member do
       get :logout
