45e3170d4f831759b25f2e97faa6285d4f902358
ddsk0512 <65224155+ddsk0512@users.noreply.github.com>
2020-08-30 17:26:15 +0900

    add enum(condition)

--- db/migrate/20200803145239_create_items.rb
+++ db/migrate/20200803145239_create_items.rb
@@ -1,17 +1,16 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
       t.timestamps
       t.string :name,             null: false
       t.text :description,        null: false
       t.string :brand,            null: false
-      t.integer :condition,       null: false
-      t.integer :status,          null: false
-      t.integer :shipping_costs,  null: false
-      t.integer :shipping_from,   null: false
-      t.integer :shipping_date,   null: false
-      t.integer :price,           null: false
+      t.integer :condition,       default: 0, null: false
+      t.integer :status,          default: 0, null: false
+      t.integer :shipping_costs,  default: 0, null: false
+      t.integer :shipping_from,   default: 0, null: false
+      t.integer :shipping_date,   default: 0, null: false
+      t.integer :price,           default: 0, null: false
     end
   end
 end
