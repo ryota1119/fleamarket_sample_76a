3246cd4d915ab746c6065859d75f329baa2a7500
ddsk0512 <65224155+ddsk0512@users.noreply.github.com>
2020-09-02 01:23:44 +0900

    fix gem&schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,12 @@ ActiveRecord::Schema.define(version: 2020_08_22_085455) do
     t.string "name", null: false
     t.text "description", null: false
     t.string "brand", null: false
-    t.integer "condition", null: false
+    t.integer "condition", default: 1, null: false
     t.integer "status", null: false
-    t.integer "shipping_costs", null: false
-    t.integer "shipping_from", null: false
-    t.integer "shipping_date", null: false
-    t.integer "price", null: false
+    t.integer "shipping_costs", default: 1, null: false
+    t.integer "shipping_from", default: 1, null: false
+    t.integer "shipping_date", default: 1, null: false
+    t.integer "price", default: 0, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
