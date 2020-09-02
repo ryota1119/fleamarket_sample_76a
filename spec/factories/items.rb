8b8a29d77fe8e1488f9b5b4ebf8f8f421a550237
fumn112026 <fumn112026@gmail.com>
2020-09-02 23:23:22 +0900

    fix_value
    可読性の向上ため、実際のユースケースが想像できるデータに変更。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   
   factory :item do
-    name           {"hoge"}
-    description    {"fuga"}
-    brand          {"piyo"}
+    name           {"MacBook Pro"}
+    description    {"1世代前のものです。新しいものを購入したので出品します。"}
+    brand          {"Apple"}
     condition      {"1"}   
     status         {"1"}   
     shipping_costs {"1"}   
     shipping_from  {"1"}   
     shipping_date  {"1"}   
-    price          {"1"}
+    price          {"10000"}
     images {[build(:image)]}   
   end
 
