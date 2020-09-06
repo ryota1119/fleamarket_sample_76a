578f82fd552bbdf9aef2a26b3264e78a98c95ee7
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-09-06 16:29:31 +0900

    edit items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,17 +5,17 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @category_parent_array = ["---"]
-    @category_parent_array = Category.where(ancestry: nil)
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def get_category_children
-  @category_children = Category.find(params[:parent_id]).category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
   def get_category_grandchildren
-    @category_grandchildren = Category.find(params[:children_id]).children_id
-  end
-end
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
   private
