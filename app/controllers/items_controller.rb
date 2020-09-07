3be95a5d1c4dba06efed816d9f7b54b7e26b88ea
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-09-07 12:51:38 +0900

    createアクション内にインスタンス変数@category_parent_arrayを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+    binding.pry
     if @item.save
       redirect_to root_path
     else
