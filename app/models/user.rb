78a1b74bc6d612e939f0ba4d06cc419b990cb044
crest89 <61658640+crest89@users.noreply.github.com>
2020-09-11 00:08:51 +0900

    updateuser_option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
-  validates :encrypted_password, presence: true, uniqueness:true, length: { minimum: 7 },
-            format: { with: /\A[a-z0-9]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, presence: true
+  validates :password, length: { minimum: 7 },
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
   validates :email, presence: true, 
             uniqueness:true, length:{ maximum: 255 }, 
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }      
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_173253) do
+ActiveRecord::Schema.define(version: 2020_08_27_170622) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
