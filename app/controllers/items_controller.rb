725bba380274386183ddc7703fc088b9060cd992
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-09-13 18:18:45 +0900

    editアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
+
   def index
   end
 
@@ -19,17 +21,30 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name)
+    @category_children_array = Category.where(ancestry: child_category.ancestry)
+    @category_grandchildren_array = Category.where(ancestry: grandchild_category.ancestry)
+  end
+
   def get_category_children
     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
   def get_category_grandchildren
     @category_grandchildren = Category.find("#{params[:child_id]}").children
-    end
   end
+  
 
   private
   
   def item_params
     params.require(:item).permit(:name, :description, :brand, :condition, :status, :shipping_costs, :shipping_from, :shipping_date, :price, :category_id, images_attributes: [:src])
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+end
