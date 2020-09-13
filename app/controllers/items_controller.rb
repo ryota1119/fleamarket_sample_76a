4b3c5fc9f19a56d1dffbb75ea346d26c798e9c2d
fumn112026 <fumn112026@gmail.com>
2020-09-13 23:55:31 +0900

    initial commit
    ルーティング、コントローラーを作成。
    仮のビューを作成。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @image = @item.images
+  end
+
   private
   
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
-  resources :items, only: [:new, :create] do
+  resources :items, only: [:new, :create, :show] do
     resources :images, only: :create
   end
   resources  :users, only: [:show, :destroy] do
