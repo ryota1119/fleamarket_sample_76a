cf50db278814f2bd5e385e177618e454c955bb98
crest89 <61658640+crest89@users.noreply.github.com>
2020-09-22 03:02:39 +0900

    error_handring

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'carrierwave'
 
 gem 'mini_magick'
 
+gem 'rails-i18n', '~> 5.1'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ApplicationRecord
         
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
   validates :password, length: { minimum: 7 },
-            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }       
-  validates :email, presence: true, 
-            uniqueness:true, length:{ maximum: 255 }, 
-            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }      
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "に半角英数字以外の文字が含まれてます。" }       
+  validates :email, uniqueness:true, length:{ maximum: 255 }, 
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "に使えない文字が入っています。" }      
   has_one :address
 end
 
