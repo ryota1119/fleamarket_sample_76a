79dff08683ba0017a4432401e4034a6145a63ac4
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-09-27 14:49:50 +0900

    画像投稿フォーム作成

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,14 +1,45 @@
 $(document).on('turbolinks:load', ()=> {
+  $(function() {
+    var file_field = document.querySelector('input[type=file]')
+    $('#img-file').change(function() {
+      var file = $('input[type="file"]').prop('files')[0];
+      var fileReader = new FileReader();
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<img src="${src}" width="114" height="80">`
+        $('#image-box__container').before(html);
+      }
+      fileReader.readAsDataURL(file);
+    });
+  });
+
+  var html= `<div class='item-image' data-image="${file.name}">
+              <div class=' item-image__content'>
+                <div class='item-image__content--icon'>
+                  <img src=${src} width="114" height="80" >
+                </div>
+              </div>
+              <div class='item-image__operetion'>
+                <div class='item-image__operetion--delete'>削除</div>
+              </div>
+            </div>`
+
+  $(document).on("click", '.item-image__operetion--delete', function(){
+    var target_image = $(this).parent().parent()
+    target_image.remove();
+    file_field.val("")
+  })
+  
   // 画像用のinputを生成する関数
-  const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="item[images_attributes][${index}][src]"
-                    id="item_images_attributes_${index}_src"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
-    return html;
-  }
+  // const buildFileField = (index)=> {
+  //   const html = `<div data-index="${index}" class="js-file_group">
+  //                   <input class="js-file" type="file"
+  //                   name="item[images_attributes][${index}][src]"
+  //                   id="item_images_attributes_${index}_src"><br>
+  //                   <div class="js-remove">削除</div>
+  //                 </div>`;
+  //   return html;
+  // }
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
