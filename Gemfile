cfe0e4ee36bd789514163737852199b1e92c4242
fumn112026 <fumn112026@gmail.com>
2020-10-01 11:57:40 +0900

    gem payjpを追加

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'mini_magick'
 
 gem 'rails-i18n', '~> 5.1'
 
+gem 'payjp'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_155013) do
+ActiveRecord::Schema.define(version: 2020_09_29_021008) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "prefecturs", default: "----", null: false
+    t.string "prefecturs", default: "0", null: false
     t.string "municipalities", null: false
     t.string "block_number", null: false
     t.string "building_name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_09_26_155013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id_id"], name: "index_credit_cards_on_user_id_id"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
