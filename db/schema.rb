dd91557e6cebb80fb5e73fa6c834f843d78d9aaa
fumn112026 <fumn112026@gmail.com>
2020-10-15 15:20:09 +0900

    siderの指摘を修正

--- db/schema.rb
+++ db/schema.rb
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_124625) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
 end
