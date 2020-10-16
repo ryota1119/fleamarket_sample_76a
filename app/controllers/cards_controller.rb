7fd2690106a341186182f09f37dfd56022b2876f
fumn112026 <fumn112026@gmail.com>
2020-10-17 00:38:54 +0900

    ローカル変数をインスタンス変数に修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -27,28 +27,28 @@ class CardsController < ApplicationController
   end
 
   def destroy #PayjpとCardデータベースを削除
-    if card.present?
+    if @card.present?
       Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
+      @card.delete
     end
       redirect_to new_card_path
   end
 
   def show #Cardのデータpayjpに送り情報を取り出す
     @item = Item.find(params[:id])
-    if card.blank?
+    if @card.blank?
       redirect_to new_card_path 
     else
       Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   private
   def set_card
-    card = Card.find_by(user_id: current_user.id)
+    @card = Card.find_by(user_id: current_user.id)
   end
 end
